79ced6295ba606261ffbf3738ae5ecb5e09c076c
phanil kumar <phanilkumar@outlook.com>
2025-09-10 13:53:25 +0530

    TDD Step 7: Negative number exception handling

--- app/models/string_calculator.rb
+++ app/models/string_calculator.rb
@@ -2,12 +2,27 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
     
+    numbers_array = parse_numbers(numbers)
+    validate_no_negatives(numbers_array)
+    numbers_array.sum
+  end
+
+  private
+
+  def parse_numbers(numbers)
     if numbers.start_with?('//')
       delimiter, numbers_part = numbers.split("\n", 2)
       custom_delimiter = delimiter[2..-1]  # Remove '//' prefix
-      numbers_part.split(custom_delimiter).map(&:to_i).sum
+      numbers_part.split(custom_delimiter).map(&:to_i)
     else
-      numbers.split(/[,\n]/).map(&:to_i).sum
+      numbers.split(/[,\n]/).map(&:to_i)
+    end
+  end
+
+  def validate_no_negatives(numbers_array)
+    negative_numbers = numbers_array.select { |n| n < 0 }
+    if negative_numbers.any?
+      raise ArgumentError, "negative numbers not allowed #{negative_numbers.join(',')}"
     end
   end
 end
