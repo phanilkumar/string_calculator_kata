d242bf147ce07387a9989c98db34ec020c893ce2
phanil kumar <phanilkumar@outlook.com>
2025-09-10 13:34:59 +0530

    TDD Step 6: Custom delimiter support + Add .gitignore

--- app/models/string_calculator.rb
+++ app/models/string_calculator.rb
@@ -1,6 +1,13 @@
 class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
-    numbers.split(/[,\n]/).map(&:to_i).sum
+    
+    if numbers.start_with?('//')
+      delimiter, numbers_part = numbers.split("\n", 2)
+      custom_delimiter = delimiter[2..-1]  # Remove '//' prefix
+      numbers_part.split(custom_delimiter).map(&:to_i).sum
+    else
+      numbers.split(/[,\n]/).map(&:to_i).sum
+    end
   end
 end
